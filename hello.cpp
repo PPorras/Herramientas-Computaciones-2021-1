1ed66433319ffd69157f3a18f352a0350cc5c8b6
Porras Air <porras@ciencias.unam.mx>
2020-12-02 19:24:16 -0600

    Push file c++

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+/*
+* Multiple line
+* comment
+*/
+#include<iostream>
+
+//Single line comment
+using namespace std;
+
+//This is where the execution of program begins
+int main()
+{
+   // displays Hello World! on screen
+   cout<<"Hello World!";
+
+   return 0;
+}
